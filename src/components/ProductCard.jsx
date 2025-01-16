780e252185c0e2f6b5bbe04364a1760f8baf57f3
Monicah Cloud <monicahcloud@gmail.com>
2025-01-16 00:14:44 -0600

    adjusted the size of the www icon

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -10,12 +10,12 @@ const ProductCard = ({ url, img, title, text }) => {
         className="w-full object-cover rounded-t-lg h-64"
       />
       <div className="capitalize p-8">
-        <h2 className='text-xl tracking-wide font-medium'>{title}</h2>
+        <h2 className='text-4xl text-cyan-600 tracking-wide font-medium'>{title}</h2>
         <p className='mt-4 text-slate-700 leading-loose'>{text}</p>
         
 <div className="mt-4 flex gap-x-4">
     <a href={url}>
-        <TbWorldWww className='h-14 w-14 text-cyan-700 hover:text-cyan-400 duration-300'/>
+        <TbWorldWww className='h-14 w-14 text-cyan-600 hover:text-cyan-400 duration-300'/>
     </a>
 </div>
       </div>
