cf2e0fed8bd8fa4b73849ef36ec0235f882c4be1
Vita Nova Designs and Marketing LLC <vitanovadesigns@Mac.attlocal.net>
2025-02-13 14:40:03 -0500

    working on merging issues

---
+++ src/components/FeaturedProductCard.jsx
@@ -0,0 +1,65 @@
+import { nanoid } from "nanoid";
+import Ebundle from "../assets/ebundle.png";
+import Renu28 from "../assets/renu28.jpeg";
+import REDOXGoldCom from "../assets/bundle.png";
+
+
+const featuredproducts = [
+  {
+    id: nanoid(),
+    img: REDOXGoldCom,
+    url: "https://shop.aseaglobal.com/products/5763?sponsorId=180SHS7BCD&m=7&st=ps",
+    title: "REDOXGold™",
+    className: "rounded-md mb-4 size-48",
+    text: "REDOXGold™ absorbs quickly for fast-acting comfort and soothing relief with lasting results. Push your body to the limits and take back control to keep doing what you love, powered by next-generation redox technology.",
+  },
+  {
+    id: nanoid(),
+    img: Ebundle,
+    url: "https://shop.aseaglobal.com/products/5584?sponsorId=180SHS7BCD&m=7&st=ps",
+    title: "Essential Bundle",
+    text: "6 bottles of ASEA Redox Cell Signaling Supplement, 1 tube of RENU28 Revitalizing Redox Gel, Shipping included",
+    className: "rounded-md mb-4 size-48",
+  },
+  {
+    id: nanoid(),
+    img: Renu28,
+    url: "https://shop.aseaglobal.com/products/3146?sponsorId=180SHS7BCD&m=7&st=ps",
+    title: "RENU28",
+    className: "rounded-md mb-4 h-52 ",
+    text: "Our skin-priming, age-fighting, cellular-rejuvenating gel powers both your skin and body to look their best so you can feel your best. Containing ASEA’s innovative redox cell signaling technology, RENU28® promotes hydration, elasticity, soothing, and skin surface cellular renewal. One simple gel, so many benefits.",
+  },
+];
+
+const FeaturedProductCard = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+        {featuredproducts.map((product) => (
+          <div
+            key={product.id}
+            className="bg-white shadow-md rounded-lg p-4 items-start hover:shadow-lg transition-shadow"
+          >
+            <img
+              src={product.img}
+              alt={product.title}
+              className={product.className}
+            />
+            <h2 className="text-lg font-semibold mb-2">{product.title}</h2>
+            <p className="text-gray-700">{product.text}</p>
+            {/* <a href={product.url} target="_blank" rel="noopener noreferrer">
+              <TbWorldWww className="h-10 w-14 text-cyan-600 hover:text-cyan-400 duration-300" />
+            </a> */}
+            <a href={product.url} target="_blank" rel="noopener noreferrer">
+              <h3 className="pt-5 text-cyan-500 font-bold ">
+                BUY HERE
+              </h3>
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProductCard;
